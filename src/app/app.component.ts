fe06f0fc12bf3c2c9f9c617e9b19561ca58a7abe
suvorova.a <suvorova.a@brainrocket.com>
2025-01-02 15:02:52 +0200

    loop, for function, fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { COURSES } from '../db-data';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,23 @@ import { CourseCardComponent } from './course-card/course-card.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Angular Course';
+  header = 'Angular Course';
 
-  coreCourse = COURSES[0];
-  rxjsCourse = COURSES[1];
-  ngrxCourse = COURSES[2];
+  courses = [...COURSES];
+  // courses = [];
+
+  // coreCourse = COURSES[0];
+  // rxjsCourse = COURSES[1];
+  // ngrxCourse = COURSES[2];
+
+  onCourseSelected(course: Course) {
+    console.log('Card is clicked', course);
+  }
+
+  // Other option to track item in object; but preferably should use simplified -  track course.id
+  // if cant track by id, can use index track $index
+
+  // trackCourse(index: number, course: Course) {
+  //   return course.id;
+  // }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  input,
+} from '@angular/core';
+import { COURSES } from '../../db-data';
+import { Course } from '../model/course';
+import { count } from 'console';
+// import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'stb-course-card',
@@ -7,6 +18,34 @@ import { Component } from '@angular/core';
   styleUrl: './course-card.component.scss',
 })
 export class CourseCardComponent {
-  // @Input()
-  // title: string;
+  @Input({
+    required: true,
+  })
+  course: Course = {
+    id: 0,
+    description: '',
+    iconUrl: '',
+    longDescription: '',
+    category: '',
+    lessonsCount: 0,
+  };
+
+  @Input({
+    required: true,
+  })
+  index: number = 0;
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  // title: string = '';
+
+  // constructor() {}
+
+  // ngOnInit() {}
+
+  onCourseViewed() {
+    console.log('Card is clicked');
+    this.courseEmitter.emit(this.course);
+  }
 }
