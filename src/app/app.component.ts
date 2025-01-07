e535b4485f217f73e999e69503d3b0f245d4e16d
suvorova.a <suvorova.a@brainrocket.com>
2025-01-07 10:24:17 +0200

    viewchild query added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { COURSES } from '../db-data';
-import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,22 +15,11 @@ export class AppComponent {
   header = 'Angular Course';
 
   courses = [...COURSES];
-  // courses = [];
 
-  title = COURSES[1].description;
-  price = 9.99396720867;
-  rate = 0.67;
-  course = COURSES[1];
-  startDate = new Date(2025, 0, 1);
+  @ViewChild(CourseCardComponent)
+  card!: CourseCardComponent;
 
   onCourseSelected(course: Course) {
-    console.log('Card is clicked', course);
+    console.log(this.card);
   }
-
-  // Other option to track item in object; but preferably should use simplified -  track course.id
-  // if cant track by id, can use index track $index
-
-  // trackCourse(index: number, course: Course) {
-  //   return course.id;
-  // }
 }
